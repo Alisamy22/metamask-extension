0d74eceab2a64f783d487d7ed9c59e5ca28af055
Filip Sekulic <filip.sekulic@consensys.net>
2022-03-25 09:18:58 +0100

    Fixed enhanced gas fee bug

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -1133,7 +1133,9 @@ export function getIsMultiLayerFeeNetwork(state) {
  * @returns Boolean
  */
 export function getAdvancedGasFeeValues(state) {
-  return state.metamask.advancedGasFee;
+  const chainId = getCurrentChainId(state);
+
+  return state.metamask.advancedGasFee[chainId];
 }
 
 export function getEIP1559V2Enabled(state) {
@@ -1148,8 +1150,11 @@ export function getEIP1559V2Enabled(state) {
  */
 export function getIsAdvancedGasFeeDefault(state) {
   const { advancedGasFee } = state.metamask;
+  const chainId = getCurrentChainId(state);
+
   return (
-    Boolean(advancedGasFee?.maxBaseFee) && Boolean(advancedGasFee?.priorityFee)
+    Boolean(advancedGasFee[chainId]?.maxBaseFee) &&
+    Boolean(advancedGasFee[chainId]?.priorityFee)
   );
 }
 
